8ff25155b4ddb7daf03e8df91f949222a9334ff9
Артем <artem_ashihmin94@mail.ru>
2021-06-14 21:59:05 +0400

    Правки массива

--- index.php
+++ index.php
@@ -82,41 +82,42 @@ $user_name = 'Аркадий'; // укажите здесь ваше имя
             <!--заполните этот список из массива с товарами-->
             <?php $announcement = 
             [
-                ['title' => '2014 Rossignol District Snowboard',
-                 'category' => 'Доски и лыжи',
-                 'price' => '10999',
-                 'URL' => 'img/lot-1.jpg'
+                [
+                    'title' => '2014 Rossignol District Snowboard',
+                    'category' => 'Доски и лыжи',
+                    'price' => '10999',
+                    'URL' => 'img/lot-1.jpg'
                 ],
-
-                ['title' => 'DC Ply Mens 2016/2017 Snowboard',
-                 'category' => 'Доски и лыжи',
-                 'price' => '159999',
-                 'URL' => 'img/lot-2.jpg'
+                [
+                    'title' => 'DC Ply Mens 2016/2017 Snowboard',
+                    'category' => 'Доски и лыжи',
+                    'price' => '159999',
+                    'URL' => 'img/lot-2.jpg'
                 ],
-
-                ['title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-                 'category' => 'Крепления',
-                 'price' => '8000',
-                 'URL' => 'img/lot-3.jpg'
+                [
+                    'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                    'category' => 'Крепления',
+                    'price' => '8000',
+                    'URL' => 'img/lot-3.jpg'
                 ],
- 
-               ['title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-                'category' => 'Ботинки',
-                'price' => '10999',
-                'URL' => 'img/lot-4.jpg'
-               ],
-
-               ['title' => 'Куртка для сноуборда DC Mutiny Charocal',
-               'category' => 'Одежда',
-               'price' => '7500',
-               'URL' => 'img/lot-5.jpg'
-               ],
-
-               ['title' => 'Маска Oakley Canopy',
-                'category' => 'Разное',
-                'price' => '5400',
-                'URL' => 'img/lot-6.jpg'
-               ],    
+                [
+                    'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                    'category' => 'Ботинки',
+                    'price' => '10999',
+                    'URL' => 'img/lot-4.jpg'
+                ],
+                [
+                    'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+                    'category' => 'Одежда',
+                    'price' => '7500',
+                    'URL' => 'img/lot-5.jpg'
+                ],
+                [
+                    'title' => 'Маска Oakley Canopy',
+                    'category' => 'Разное',
+                    'price' => '5400',
+                    'URL' => 'img/lot-6.jpg'
+                ],    
             ];?>
 
             <?php foreach ($announcement as $key => $value): ?>
